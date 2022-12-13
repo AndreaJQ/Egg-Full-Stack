70b6f30017086032db17a9a10b40a9f38185443d
AndreaJQQ <andreagjalil@gmail.com>
2022-12-12 21:37:07 -0300

    Guia 10 manos a la obra1

---
+++ GUIA 10 HERENCIA/Guia10ManosAlaObra1/src/main/java/andrea/guia10manosalaobra1/Gato.java
@@ -0,0 +1,13 @@
+
+package andrea.guia10manosalaobra1;
+
+
+public class Gato extends Animal{
+    @Override
+    public void hacerRuido(){
+        super.hacerRuido();
+        System.out.println("MIAU");
+    }
+
+    
+}
---
+++ GUIA 10 HERENCIA/Guia10ManosAlaObra1/src/main/java/andrea/guia10manosalaobra1/Perro.java
@@ -0,0 +1,14 @@
+
+package andrea.guia10manosalaobra1;
+
+public class Perro extends Animal{
+
+    @Override
+    public void hacerRuido() {
+        super.hacerRuido();
+       System.out.println("wuau");
+    }
+
+   
+
+}
