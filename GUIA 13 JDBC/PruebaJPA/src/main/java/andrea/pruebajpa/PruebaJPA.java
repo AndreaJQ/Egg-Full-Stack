a28a3b3c73fb0bfd2e3b43962f2f78a654faabcf
Andrea Jalil <andreajalil@MBP-Andrea.local>
2023-02-14 22:12:14 -0300

    prueba

---
+++ GUIA 13 JDBC/PruebaJPA/src/main/java/andrea/pruebajpa/PruebaJPA.java
@@ -0,0 +1,11 @@
+
+package andrea.pruebajpa;
+
+import andrea.pruebajpa.persistencia.ControladoraPersistencia;
+
+public class PruebaJPA {
+
+    public static void main(String[] args) {
+        ControladoraPersistencia CP =new ControladoraPersistencia();
+    }
+}
